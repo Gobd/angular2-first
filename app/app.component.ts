b9a20f75029eff03cfd1a73b8c7e33f10967d2c4
Brian Kemper <bkemper@gmail.com>
2016-04-05 10:23:54 -0600

    finished step 3

--- app/app.component.ts
+++ app/app.component.ts
@@ -56,30 +56,32 @@ export class Hero {
       }
     `],
     selector: 'my-app',
-    template: `
+    template:`
     <h1>{{title}}</h1>
-        <h2>My Heroes</h2>
-        <ul class="heroes">
-          <li *ngFor="#hero of heroes">
-             <span class="badge">{{hero.id}}</span> {{hero.name}}
-          </li>
-        </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
         <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>
-    `
+  `
 })
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-    public heroes = HEROES;
+    onSelect(hero: Hero) { this.selectedHero = hero; }
+    selectedHero: Hero;
+    heroes = HEROES;
 }
 
 var HEROES: Hero[] = [
